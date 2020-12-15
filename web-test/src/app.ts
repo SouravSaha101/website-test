ad8dde50f14fe3ff493762666d00c8365a89a75b
Sourav <souravsaha101@gmail.com>
2020-12-15 20:08:28 +0530

    p

---
+++ web-test/src/app.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    template: require('./app.html'),
+    components: {
+    }
+})
+export class App extends Vue {
+
+}
+
+
+
+
