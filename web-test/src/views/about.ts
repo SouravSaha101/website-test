af898532af3816caaa3b29187847e8845f62d485
Sourav <souravsaha101@gmail.com>
2020-12-15 20:33:28 +0530

    code changes

---
+++ web-test/src/views/about.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    template: require('./about.html'),
+    components: {
+    }
+})
+export class About extends Vue {
+
+}
